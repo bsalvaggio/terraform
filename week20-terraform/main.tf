0591b1fab3ac235a232302095dd42e883fa29ebc
Bill Salvaggio <119374432+bsalvaggio@users.noreply.github.com>
2023-04-03 21:38:42 -0400

    update to working version (basic)

--- week20-terraform/main.tf
+++ week20-terraform/main.tf
@@ -1,6 +1,7 @@
-# Get the current region and availability zones
-data "aws_region" "current" {}
-data "aws_availability_zones" "available" {}
+provider "aws" {
+  region = "us-east-1"
+}
+
 # Create a VPC
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
@@ -8,17 +9,19 @@ resource "aws_vpc" "vpc" {
     Name = "my-vpc"
   }
 }
+
 # Create a subnet
 resource "aws_subnet" "subnet" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[0]
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
     Name = "my-subnet"
   }
 }
+
 # Create an internet gateway
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.vpc.id
@@ -26,6 +29,7 @@ resource "aws_internet_gateway" "igw" {
     Name = "my-internet-gateway"
   }
 }
+
 # Create a route table
 resource "aws_route_table" "route_table" {
   vpc_id = aws_vpc.vpc.id
@@ -37,27 +41,39 @@ resource "aws_route_table" "route_table" {
     Name = "my-route-table"
   }
 }
+
 # Associate the route table with the subnet
 resource "aws_route_table_association" "route_table_association" {
   subnet_id      = aws_subnet.subnet.id
   route_table_id = aws_route_table.route_table.id
 }
+
 # Create a security group
 resource "aws_security_group" "security_group" {
   name        = "my-security-group"
-  description = "Allow inbound traffic"
+  description = "Allow all inbound traffic and HTTP/HTTPS outbound traffic"
   vpc_id      = aws_vpc.vpc.id
 
+  # Allow all inbound traffic from any IP address
   ingress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    protocol    = "-1"          # Allow all protocols
+    cidr_blocks = ["0.0.0.0/0"] # Allow from any IP address
+  }
+
+  # Allow outbound traffic on all ports and protocols
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"          # Allow all protocols
+    cidr_blocks = ["0.0.0.0/0"] # Allow to any IP address
   }
 }
+
 # Create an EC2 instance
 resource "aws_instance" "instance" {
-  ami                    = "ami-00c39f71452c08778" # Amazon Linux 2023 AMI (HVM), SSD Volume Type
+  ami                    = "ami-00c39f71452c08778" # Amazon Linux 2023 AMI
   instance_type          = "t2.micro"
   subnet_id              = aws_subnet.subnet.id
   vpc_security_group_ids = [aws_security_group.security_group.id]
@@ -66,7 +82,15 @@ resource "aws_instance" "instance" {
   }
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, World!" > index.html
-              nohup busybox httpd -f -p 8080 &
+              sudo yum -y update
+              sudo yum -y install httpd
+              sudo systemctl enable httpd
+              sudo systemctl start httpd
               EOF
+}
+
+# Output the pulic IP address of the EC2 instance
+output "instance_public_ip" {
+  value       = aws_instance.instance.public_ip
+  description = "The public IP address of the EC2 instance"
 }
